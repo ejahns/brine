0e71dec4601394068331225e2069f1ceac20da34
Ethan Jahns <ejahns@digitalmeasures.com>
2017-05-04 17:02:18 -0500

    added test for improperly placed tag

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.github.ejahns.ParserException;
 import com.github.ejahns.PickleJar;
 import com.github.ejahns.model.Feature;
 import com.google.gson.Gson;
@@ -23,5 +24,18 @@ public class Tests {
 		JsonParser jsonParser = new JsonParser();
 		JsonElement parse1 = jsonParser.parse(reader);
 		Assert.assertEquals(parse1, gson.toJsonTree(ferment));
+	}
+
+	@Test
+	public void badFeature() throws IOException {
+		File file = new File("src/test/resources/sample_feature_bad.feature");
+		try {
+			Feature ferment = PickleJar.cure(file);
+		}
+		catch (ParserException.UnexpectedTokenException e) {
+			Assert.assertEquals(e.getMessage(), "Expected one of EOF, StepLine, TagLine, ScenarioLine, ScenarioOutlineLine, Other but got TagLine: @This doesn't go here!!! at line 12");
+			return;
+		}
+		throw new IllegalStateException();
 	}
 }
---
+++ src/test/resources/sample_feature_bad.feature
@@ -0,0 +1,19 @@
+@Some Tag
+@Meta("Some tag with an argument")
+Feature: Some Feature
+  Some description.
+
+  Background:
+    Given some common condition
+    And something else
+
+  Scenario: Some Scenario
+  Some scenario description.
+  @This doesn't go here!!!
+    When I do something
+    Then there is some result
+
+  Scenario: Some other scenario
+
+    When I do something else
+    Then something else happens
