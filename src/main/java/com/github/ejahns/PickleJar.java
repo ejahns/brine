da16f3a218f1351bfdb8dd1cc4a795ba7ff47362
Ethan Jahns <ejahns@digitalmeasures.com>
2017-05-04 16:36:26 -0500

    ferment() -> cure()

--- src/main/java/com/github/ejahns/PickleJar.java
+++ src/main/java/com/github/ejahns/PickleJar.java
@@ -10,18 +10,18 @@ import com.github.ejahns.model.Feature;
 
 public class PickleJar {
 
-	public static Feature ferment(Reader reader) {
+	public static Feature cure(Reader reader) {
 		TokenQueue tokenQueue = new TokenQueue(reader);
 		return new Parser().parse(tokenQueue);
 	}
 
-	public static Feature ferment(File file) throws FileNotFoundException {
-		Feature ferment = ferment(new FileReader(file));
+	public static Feature cure(File file) throws FileNotFoundException {
+		Feature ferment = cure(new FileReader(file));
 		ferment.setUri(file.toURI());
 		return ferment;
 	}
 
-	public static Feature ferment(URI uri) throws FileNotFoundException {
-		return ferment(new File(uri));
+	public static Feature cure(URI uri) throws FileNotFoundException {
+		return cure(new File(uri));
 	}
 }
